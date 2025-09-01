b8445e632b9ee6b80ed9c8449e17d5cb4ddd6e01
mitugui <cristian.mitugui04@gmail.com>
2025-09-01 12:27:32 -0300

    chore: add initial Post and Comments records to seeds

--- my_ruby_app/db/seeds.rb
+++ my_ruby_app/db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Post.create!([
+  { title: "First Post", context: "This is the body of the first post." },
+  { title: "Second Post", context: "This is the body of the second post." },
+  { title: "Third Post", context: "This is the body of the third post." }
+])
+
+Comment.create!([
+    { content: "Great post!", post_id: Post.find_by(title: "First Post").id },
+    { content: "Very informative.", post_id: Post.find_by(title: "First Post").id }
+])
